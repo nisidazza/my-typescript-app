187c3e9ff13f5e8a2a54395e0fa5dff3f809e0e8
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-08-07 09:23:19 +1200

    add LoginForm test file

--- src/__tests__/LoginForm.test.tsx
+++ src/__tests__/LoginForm.test.tsx
@@ -7,6 +7,39 @@ import { render, fireEvent, waitForElement } from "@testing-library/react";
 
 import LoginForm, { Props } from "../LoginForm";
 
+//render helper function to handle props overriding
+/*/f the Props interface changes, TypeScript will throw a compiler error and the test helper will need to be updated, 
+ensuring our tests are kept updated.*/
+function renderLoginForm(props: Partial<Props> = {}) {
+    const defaultProps: Props = {
+        onPasswordChange() {
+            return;
+        },
+        onRememberChange() {
+            return;
+        },
+        onUsernameChange() {
+            return;
+        },
+        onSubmit(){
+            return;
+        },
+        shouldRemember: true
+    };
+    return render(<LoginForm {...defaultProps} {...props} />)
+}
+
 describe("<LoginForm />", () => {
-  test("should display a blank login form, with remember me checked by default", async () => {});
+  test("should display a blank login form, with remember me checked by default", async () => {
+      const {findByTestId} = renderLoginForm();
+
+      const loginForm = await findByTestId("login-form");
+
+      expect(loginForm).toHaveFormValues({
+          username: "",
+          password: "",
+          remember: true
+      });
+  });
+  
 });
